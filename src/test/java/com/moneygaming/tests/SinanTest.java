1b3855f623155f26bd8f1a5c73ba329bfc52fc0a
sinanygt <sinanyigit8@gmail.com>
2021-09-22 22:29:43 +0200

    sinan branch commit

---
+++ src/test/java/com/moneygaming/tests/SinanTest.java
@@ -0,0 +1,14 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class SinanTest {
+
+    @Test
+    public  void testSinan(){
+
+        System.out.println("my nice test");
+    }
+
+
+}
